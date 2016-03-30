36d50310e6f87b8d375d776ede61c96b8656c74c
Eric Faehnrich <misterfright@gmail.com>
2016-03-30 10:50:37 -0400

    lazyfoo 7, use textures

--- spacemurder.c
+++ spacemurder.c
@@ -1,7 +1,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h>
+//#include <SDL2/SDL_ttf.h>
+//#include <SDL2/SDL_mixer.h>
 //for unix
 
 #include <stdio.h>
@@ -11,12 +11,18 @@
 
 int main() {
     SDL_Window *Window = NULL;
-    SDL_Surface *Surface = NULL;
-    SDL_Surface *CraftSurface = NULL;
+    //SDL_Surface *Surface = NULL;
+    //SDL_Surface *CraftSurface = NULL;
+    SDL_Texture *CraftTexture = NULL;
+    SDL_Renderer *WindowRenderer = NULL;
 
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize: %s\n", SDL_GetError());
         return -1;
+    }
+    
+    if(!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+        printf("Linear tecture filtering not enabled.\n");
     }
 
     Window = SDL_CreateWindow("Space Murder",
@@ -31,29 +37,34 @@ int main() {
         return -1;
     }
 
+    WindowRenderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED);
+    if(NULL == WindowRenderer) {
+        printf("Renderer could not be created: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    SDL_SetRenderDrawColor(WindowRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
     int imgFlags = IMG_INIT_PNG;
     if(!(IMG_Init(imgFlags) & imgFlags)) {
         printf("SDL_image cuold not initialize: %s\n", IMG_GetError());
         return -1;
     }
 
-    Surface = SDL_GetWindowSurface(Window);
-
     {
-    SDL_Surface *UnoptimizedCraftSurface = NULL;
-    UnoptimizedCraftSurface = IMG_Load("spacemurdercraft.png");
-    if(NULL == UnoptimizedCraftSurface) {
-        printf("Could not laod spacemurdercraft.bmp: %s\n", SDL_GetError());
+    SDL_Surface *loadedCraftSurface = IMG_Load("spacemurdercraft.png");
+    if(NULL == loadedCraftSurface) {
+        printf("Could not laod spacemurdercraft.png: %s\n", SDL_GetError());
         return -1;
     }
 
-    CraftSurface = SDL_ConvertSurface(UnoptimizedCraftSurface, Surface->format, 0);
-    if(NULL == CraftSurface) {
-        printf("Unable to optimize image: %s\n", SDL_GetError());
+    CraftTexture = SDL_CreateTextureFromSurface(WindowRenderer, loadedCraftSurface);
+    if(NULL == CraftTexture) {
+        printf("Unable to create texture from surface: %s\n", SDL_GetError());
         return -1;
     }
     
-    SDL_FreeSurface(UnoptimizedCraftSurface);
+    SDL_FreeSurface(loadedCraftSurface);
     }
     
     int keep_going = 1;
@@ -65,8 +76,9 @@ int main() {
                 keep_going = 0;
             }
         }
-        SDL_BlitSurface(CraftSurface, NULL, Surface, NULL);
-        SDL_UpdateWindowSurface(Window);
+        SDL_RenderClear(WindowRenderer);
+        SDL_RenderCopy(WindowRenderer, CraftTexture, NULL, NULL);
+        SDL_RenderPresent(WindowRenderer);
     }
     
     return 0;
