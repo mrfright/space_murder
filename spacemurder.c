fa7c980d52bd7b2b55ec00f4868d0af7688c9ed5
Eric Faehnrich <misterfright@gmail.com>
2016-06-09 22:15:49 -0400

    linux added boundry

--- spacemurder.c
+++ spacemurder.c
@@ -1,15 +1,15 @@
-/*
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-*/
+
 //#include <SDL2/SDL_ttf.h>
 //#include <SDL2/SDL_mixer.h>
 //for unix
 
 
 //for windows
-#include "SDL_win\SDL.h"
-#include "SDL_win\SDL_image.h"
+//#include "SDL_win\SDL.h"
+//#include "SDL_win\SDL_image.h"
 
 #include <stdio.h>
 
